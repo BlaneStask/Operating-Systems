acd69ac51c1fdf0516131a90fae48265cd279f68
Blane Staskiewicz <bas6248@psu.edu>
2021-02-06 12:47:15 -0500

    Add files via upload

---
+++ spawn-child-process.java
@@ -0,0 +1,52 @@
+/*
+Name: Blane Staskiewicz
+Course: CMPSC 472 Section 001
+Assignment: Lab 1
+Purpose: This program starts two other processes (Notes and Calculator). 
+This program also checks the status of these processes. (on MacOS)
+*/ 
+
+package spawn_childprocesses_reqts;
+import java.util.*;
+
+public class driver
+{ 
+	/** This method is the main/driver method
+	 * 
+	 * @throws Exception e   
+	 */
+	public static void main(String[] args)
+	{
+        try 
+        { 
+            // create two new processes 
+        	// on macOS:
+            System.out.println("Creating Processes"); 
+            // Process p = Runtime.getRuntime().exec("open -Fa calculator");
+            // Process p2 = Runtime.getRuntime().exec("open -Fa notes");
+            // on windowsOS:
+            Process p = Runtime.getRuntime().exec("C:\\Windows\\System\\calc.exe");
+            Process p2 = Runtime.getRuntime().exec("C:\\Windows\\System\\notepad.exe");
+      
+            // check if processes are running
+            if(!p.isAlive()) {
+            	System.out.println("Error calculator process is not running"); 
+            }
+            if(!p2.isAlive()) {
+            	System.out.println("Error notes process is not running"); 
+            }
+            
+            // wait until processes are terminated
+            p.waitFor();
+            p2.waitFor(); 
+            
+            // checking the exit value of subprocesses 
+            System.out.println("Calculator exit value: " + p.exitValue()); 
+            System.out.println("Notes exit value: " + p2.exitValue()); 
+        }  
+        catch (Exception e)  
+        { 
+            e.printStackTrace(); 
+        } 
+	}
+}
